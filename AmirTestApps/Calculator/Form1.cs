77221bbf3c74723943300ef2aadf9ccbdf8af673
Amirvashahri <amir.vashahri@gmail.com>
2024-11-12 14:14:50 +0330

    Add files via upload

---
+++ AmirTestApps/Calculator/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void zero(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dot(object sender, EventArgs e)
+        {
+
+        }
+
+        private void C(object sender, EventArgs e)
+        {
+
+        }
+
+        private void plus(object sender, EventArgs e)
+        {
+
+        }
+
+        private void one(object sender, EventArgs e)
+        {
+
+        }
+
+        private void two(object sender, EventArgs e)
+        {
+
+        }
+
+        private void three(object sender, EventArgs e)
+        {
+
+        }
+
+        private void four(object sender, EventArgs e)
+        {
+
+        }
+
+        private void five(object sender, EventArgs e)
+        {
+
+        }
+
+        private void six(object sender, EventArgs e)
+        {
+
+        }
+
+        private void seven(object sender, EventArgs e)
+        {
+
+        }
+
+        private void eight(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nine(object sender, EventArgs e)
+        {
+
+        }
+
+        private void multi(object sender, EventArgs e)
+        {
+
+        }
+
+        private void division(object sender, EventArgs e)
+        {
+
+        }
+
+        private void minus(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
