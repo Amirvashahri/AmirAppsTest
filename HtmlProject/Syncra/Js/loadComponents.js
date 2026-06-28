a6543f5733e177002b32555d639f9eafef726c45
Amirvashahri <amir.vashahri@gmail.com>
2026-06-28 22:52:36 +0330

    Edit the header Js and css code

--- HtmlProject/Syncra/Js/loadComponents.js
+++ HtmlProject/Syncra/Js/loadComponents.js
@@ -4,6 +4,7 @@ fetch('/Pages/header.html')
 
     document.getElementById('header').innerHTML = data;
 
+    // Active Page
     const currentPage = window.location.pathname
       .split("/")
       .pop();
@@ -22,11 +23,39 @@ fetch('/Pages/header.html')
 
     });
 
+    // =========================
+    // Resources Dropdown
+    // =========================
+
+    const dropdown = document.querySelector(".nav-dropdown");
+    const dropdownBtn = document.querySelector(".nav-dropdown-btn");
+
+    if (dropdown && dropdownBtn) {
+
+      dropdownBtn.addEventListener("click", function (e) {
+
+        e.preventDefault();
+        e.stopPropagation();
+
+        dropdown.classList.toggle("active");
+
+      });
+
+      document.addEventListener("click", function () {
+
+        dropdown.classList.remove("active");
+
+      });
+
+    }
+
   });
 
 fetch('/Pages/footer.html')
   .then(response => response.text())
   .then(data => {
+
     document.getElementById('footer').innerHTML = data;
+
   })
   .catch(error => console.error('Footer Error:', error));
